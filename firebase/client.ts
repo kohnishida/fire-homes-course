94cd450e208b6a86bdc5ada67cd0ec21d0b381f2
Ko Nishida <konishida@KonoMacBook-Pro.local>
2025-06-24 15:04:58 +0200

    first commit

---
+++ firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdJx--x3OuASN1y1W-WLTvTO7_7GINZdM",
+  authDomain: "fire-homes-course-bc4bc.firebaseapp.com",
+  projectId: "fire-homes-course-bc4bc",
+  storageBucket: "fire-homes-course-bc4bc.firebasestorage.app",
+  messagingSenderId: "805994910840",
+  appId: "1:805994910840:web:7aee1b2b1f69fb8cf4898d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
