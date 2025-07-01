1a284604313b39a6f85a9241bfb8b29fc3543bcc
Ko Nishida <konishida@KonoMacBook-Pro.local>
2025-07-01 11:32:49 +0200

    add admin page and breadcrumbs component]

---
+++ app/admin-dashboard/new/page.tsx
@@ -0,0 +1,18 @@
+import { Breadcrumbs } from "@/components/ui/breadcrumb";
+
+export default function NewProperty() {
+  return(
+    <div>
+      <Breadcrumbs items={[
+        {
+          href: "/admin-dashboard",
+          label: "Dashboard"
+        },
+        {
+          label: "New Property"
+        }
+      ]}>
+      </Breadcrumbs>
+    </div>
+  )
+}
--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,5 +1,22 @@
+import { Breadcrumbs } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function AdminDashboard() {
   return (
-    <div>AdminDashboard</div>
-  )
+    <div>
+      <Breadcrumbs
+        items={[
+          { label: "Dashboard" },
+        ]}
+      ></Breadcrumbs>
+      <h1 className="text-4xl font-bold mt-6">Admin Dashboard</h1>
+      <Button asChild className="inline-flex pl-2 gap-2 mt-4">
+        <Link href="/admin-dashboard/new">
+          <PlusCircleIcon/> New Property
+        </Link>
+      </Button>
+    </div>
+  );
 }
