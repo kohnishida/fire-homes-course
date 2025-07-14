ba12e70c93a5608ccf9ce5151512799329af2f06
Ko Nishida <konishida@KonoMacBook-Pro.local>
2025-07-14 11:37:23 +0200

    Style status badge and pagination

--- app/admin-dashboard/properties-table.tsx
+++ app/admin-dashboard/properties-table.tsx
@@ -1,8 +1,11 @@
+import PropertySatusBadge from "@/components/property-status-badge";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getProperties } from "@/data/properties";
-import { PencilIcon } from "lucide-react";
+import { EyeIcon, PencilIcon } from "lucide-react";
 import Link from "next/link";
+import numeral from "numeral";
+
 
 export default async function PropertiesTable ({
   page = 1
@@ -50,18 +53,19 @@ export default async function PropertiesTable ({
               return (
                 <TableRow key={property.id}>
                   <TableCell>{address}</TableCell>
-                  <TableCell>{property.price}</TableCell>
-                  <TableCell>{property.status}</TableCell>
                   <TableCell>
+                    €{numeral(property.price).format("0,0")}
+                  </TableCell>
+                  <TableCell><PropertySatusBadge status={property.status} /></TableCell>
+                  <TableCell className="flex justify-end gap-1">
                     <Button asChild variant={"outline"} size="sm">
-                      <Link href={`/admin-dashboard/view/${property.id}`}>
-                        view
+                      <Link href={`/property/${property.id}`}>
+                        <EyeIcon />
                       </Link>
-                    </Button>{" "}
-                    /{" "}
+                    </Button>
                     <Button asChild variant={"outline"} size="sm">
                       <Link href={`/admin-dashboard/edit/${property.id}`}>
-                        <PencilIcon></PencilIcon>
+                        <PencilIcon />
                       </Link>
                     </Button>
                   </TableCell>
@@ -74,7 +78,13 @@ export default async function PropertiesTable ({
               <TableCell colSpan={4} className="text-center">
                 {Array.from({length: totalPages}).map((_, i) => {
                   return (
-                    <Button key={i} asChild variant="outline" className="mx-1">
+                    <Button
+                      disabled={page === i + 1}
+                      key={i}
+                      asChild={page !== i + 1}
+                      variant="outline"
+                      className="mx-1"
+                    >
                       <Link href={`/admin-dashboard?page=${i + 1}`}>
                         {i + 1}
                       </Link>
