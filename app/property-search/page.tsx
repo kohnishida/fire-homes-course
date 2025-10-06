85b8c338540190109f8d87bf5601467683ec1700
Ko Nishida <konishida@KonoMacBook-Pro.local>
2025-10-06 12:07:16 +0200

    Query fitered properties

--- app/property-search/page.tsx
+++ app/property-search/page.tsx
@@ -1,8 +1,40 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import FiltersForm from "./filters-form";
 import { Suspense } from "react";
+import { getProperties } from "@/data/properties";
 
-export default function PropertySearch() {
+export default async function PropertySearch({ 
+  searchParams
+ }: { 
+  searchParams : Promise<any> 
+}) {
+  const searchParamsValues = await searchParams;
+
+  const parsedPage = parseInt(searchParamsValues?.page);
+  const parsedMinPrice = parseInt(searchParamsValues?.minPrice);
+  const parsedMaxPrice = parseInt(searchParamsValues?.maxPrice);
+  const parsedMinBedrooms = parseInt(searchParamsValues?.minBedrooms);
+
+  const page = isNaN(parsedPage) ? 1 : parsedPage;
+  const minPrice = isNaN(parsedMinPrice) ? null : parsedMinPrice;
+  const maxPrice = isNaN(parsedMaxPrice) ? null : parsedMaxPrice;
+  const minBedrooms = isNaN(parsedMinBedrooms) ? null : parsedMinBedrooms;
+
+  const  porperties = await getProperties({
+    filters: {
+      minPrice,
+      maxPrice,
+      minBedrooms,
+      status: ["for-sale"],
+    },
+    pagination: {
+      page,
+      pageSize: 3,
+    },
+  });
+
+  console.log(porperties);
+
   return (
     <div className="max-w-screen-lg mx-auto">
       <h1 className="text-4xl font-bold p-5">Property Search</h1>
