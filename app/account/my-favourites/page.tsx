eb1293e62f01682410e67138fa4a7a03d123fb64
Ko Nishida <konishida@KonoMacBook-Pro.local>
2025-12-11 13:18:59 +0100

    render the favourites list

--- app/account/my-favourites/page.tsx
+++ app/account/my-favourites/page.tsx
@@ -1,5 +1,17 @@
+import PropertyStatusBadge from "@/components/property-status-badge";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { getUserFavourites } from "@/data/favourites";
 import { getPropertiesByIds } from "@/data/properties";
+import { EyeIcon, Trash2Icon } from "lucide-react";
+import Link from "next/link";
 
 export default async function MyFavourites({
   searchParams,
@@ -19,8 +31,67 @@ export default async function MyFavourites({
   );
 
   const properties = await getPropertiesByIds(paginatedFavourites);
-  console.log({paginatedFavourites, properties});
+  console.log({ paginatedFavourites, properties });
 
+  return (
+    <div className="max-w-screen-lg mx-auto">
+      <h1 className="text-4xl font-bold py-4 mt-5">My favourites</h1>
+      {!paginatedFavourites.length && (
+        <h2 className="text-center text-zinc-400 text-3xl font-bold py-10">
+          You have no favourited properties
+        </h2>
+      )}
+      {!!paginatedFavourites.length && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Property</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead />
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {paginatedFavourites.map((favaourite) => {
+              const property = properties.find(
+                (property) => property.id === favaourite
+              );
+              const address = [
+                property?.address1,
+                property?.address2,
+                property?.city,
+                property?.postcode,
+              ]
+                .filter((addressLine) => !!addressLine)
+                .join(", ");
 
-  return <div>My Favourite Page</div>;
+              return (
+                <TableRow key={favaourite}>
+                  <TableCell>{address}</TableCell>
+                  <TableCell>
+                    {!!property && (
+                      <PropertyStatusBadge status={property?.status} />
+                    )}
+                  </TableCell>
+                  <TableCell className="flex justify-end gap-1">
+                    {!!property && (
+                      <>
+                        <Button asChild variant="outline">
+                          <Link href={`/property/${property.id}`}>
+                            <EyeIcon />
+                          </Link>
+                        </Button>
+                        <Button variant="outline">
+                          <Trash2Icon />
+                        </Button>
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      )}
+    </div>
+  );
 }
